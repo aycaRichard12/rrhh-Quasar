b766287ee4f5c3684e05fe536cb08c7d5980ebb0
ronly-v <gojeronly@gmail.com>
2026-05-26 12:41:31 -0400

    Creacion de carpetas de modulos con solo paginas para prueba de vista del submenu

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,18 +7,38 @@ const routes: RouteRecordRaw[] = [
     redirect: '/auth/login',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: 'dashboard',       component: () => import('pages/IndexPage.vue') },
-      { path: 'welcome',         component: () => import('pages/WelcomePage.vue') },
+      { path: 'dashboard', component: () => import('pages/IndexPage.vue') },
+      { path: 'welcome', component: () => import('pages/WelcomePage.vue') },
+
       // CONFIGURACION-Organizacion
-      { path: 'areas',           component: () => import('src/modules/areas/pages/AreasPage.vue') },
+      { path: 'areas', component: () => import('src/modules/areas/pages/AreasPage.vue') },
       // CONFIGURACION-Beneficios Sociales
       { path: 'entesReguladores',component: () => import('src/modules/entesReguladores/pages/EntesReguladoresPage.vue') },
-      { path: 'beneficios',      component: () => import('src/modules/beneficios/pages/BeneficiosPage.vue') },
+      { path: 'beneficios', component: () => import('src/modules/beneficios/pages/BeneficiosPage.vue') },
+      // CONFIGURACION-Valoración
+      { path: 'metodosdeevaluacion', component: () => import('src/modules/metodosdeevaluacion/pages/MetodosDeEvaluacionPage.vue') },
+      // CONFIGURACION-Sanciones
+      { path: 'niveles', component: () => import('src/modules/niveles/pages/NivelesPage.vue') },
       // CONFIGURACION-Remuneración
-      { path: 'bonosEmpresa',    component: () => import('src/modules/bonosEmpresa/pages/BonosEmpresaPage.vue') },
+      { path: 'metodosdepago', component: () => import('src/modules/metodosdepago/pages/MetodosDePagoPage.vue') },
+      { path: 'bonosEmpresa', component: () => import('src/modules/bonosEmpresa/pages/BonosEmpresaPage.vue') },
+      
       // GESTIÓN RECLUTAMIENTO-Admisión
-      { path: 'convocatorias',   component: () => import('src/modules/convocatorias/pages/convocatoriasPage.vue') },
+      { path: 'convocatorias', component: () => import('src/modules/convocatorias/pages/convocatoriasPage.vue') },
 
+      //TRABAJADOR-Personal
+      { path: 'trabajadores', component: () => import('src/modules/trabajadores/pages/TrabajadoresPage.vue') },
+      //TRABAJADOR-Novedades
+      { path: 'permisos', component: () => import('src/modules/permisos/pages/PermisosPage.vue') },
+      //TRABAJADOR-Procesos Laborales
+      { path: 'sanciones', component: () => import('src/modules/sanciones/pages/SancionesPage.vue') },
+
+      //GESTIÓN DE CAPACITACIÓN-Evento
+
+      //GESTIÓN SALARIAL-Planillas
+      { path: 'sueldosysalarios', component: () => import('src/modules/sueldosysalarios/pages/SueldosYSalariosPage.vue') },
+      //GESTIÓN SALARIAL-Boletas
+      { path: 'salarios', component: () => import('src/modules/salarios/pages/SalariosPage.vue') },
     ],
   },
   {
